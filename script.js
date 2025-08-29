254565558908d61b3932ec9b802a5123daac77f0
one080310 <joyewon10345@gmail.com>
2025-08-29 17:14:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const API_KEY = "회원님_API_KEY";
+const city = "Seoul";
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+  .then(res => res.json())
+  .then(data => {
+    const weatherDiv = document.getElementById("weather");
+    weatherDiv.innerText = `${data.name}의 현재 온도: ${data.main.temp}°C`;
+  })
+  .catch(err => console.error(err));
